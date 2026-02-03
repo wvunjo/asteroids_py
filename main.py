d06be1433615aaf7ef4ce46331989e02281eac72
wvunjo <joshua.webb4213@gmail.com>
2026-02-03 11:55:26 -0500

    D: - main.py ordering changed within the loop along with adding import for player and placing onto the screen - constants.py added PLAYER_RADIUS and LINE_WIDTH - circleshape.py file created - player.py file created setting up a Player class that inherits from circleshape.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
 	pygame.init()
 	clock = pygame.time.Clock()
 	dt = 0
-	
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	
 	print("Starting Asteroids with pygame version:", pygame.version.ver)
 	print("Screen width:", SCREEN_WIDTH)
@@ -16,13 +17,15 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		dt = clock.tick(60) / 1000
-		log_state()
-		screen.fill("black")
-		pygame.display.flip()
-	
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		log_state()
+		screen.fill("black")
+		player.draw(screen)
+		pygame.display.flip()
+	
+		
 
 
 	
