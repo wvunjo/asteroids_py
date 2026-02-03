c4ee2c7bb484079cb1100351cab99ed107383aee
wvunjo <joshua.webb4213@gmail.com>
2026-02-03 11:10:20 -0500

    C: Added FPS

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from logger import log_state
 
 def main():
 	pygame.init()
-	
+	clock = pygame.time.Clock()
+	dt = 0
 	
 	
 	print("Starting Asteroids with pygame version:", pygame.version.ver)
@@ -14,6 +15,7 @@ def main():
 	
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
+		dt = clock.tick(60) / 1000
 		log_state()
 		screen.fill("black")
 		pygame.display.flip()
