19ca148330f13262a23d5d8f9e24abf6a4286f78
wvunjo <joshua.webb4213@gmail.com>
2026-02-03 10:04:12 -0500

    A: - Setup asteroids_py workspace - added game_state.jsonl to .gitignore - added logger.py file - added constants.py file - made initial setup for main.py so it opens a 1280 x 720 black screen and can be exited by system exit button of the window - added uv.lock which confirms the exact versions of all dependancies - pyproject.toml: This file is used for Python project configuration, including dependencies and build settings. - README.md is created but current contains no contents at this time - added .python-version: This file specifies the Python version to be used by tools like pyenv

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+	pygame.init()
+	
+	
+	
+	print("Starting Asteroids with pygame version:", pygame.version.ver)
+	print("Screen width:", SCREEN_WIDTH)
+	print("Screen height:", SCREEN_HEIGHT)
+
+	
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	while True:
+		log_state()
+		screen.fill("black")
+		pygame.display.flip()
+	
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+
+
+	
+
+if __name__ == "__main__":
+    main()
