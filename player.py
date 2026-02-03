c3fb2e697a092898ea9da8d55b2fe5591c83e128
wvunjo <joshua.webb4213@gmail.com>
2026-02-03 12:57:20 -0500

    F: - constants.py added PLAYER_SPEED - player.py added move method to Player class including vector math and added to update method to call the move method for keys W and S

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 class Player(CircleShape):
@@ -30,4 +30,14 @@ class Player(CircleShape):
             self.rotation -= PLAYER_TURN_SPEED * dt
         if keys[pygame.K_d]:
             self.rotation += PLAYER_TURN_SPEED * dt
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+        
             
