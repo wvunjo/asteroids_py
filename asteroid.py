e0bf0b8733ef19f0dbfb743e635e8801724ec786
wvunjo <joshua.webb4213@gmail.com>
2026-02-03 14:21:55 -0500

    H: - asteroid.py file created and created Asteroid class inheriting from CircleShape overiding draw and update - asteroidfield.py file created and AsteroidField class containing logic for spawning new asteroids - constants.py updated with ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE_SECONDS, ASTEROID_MAX_RADIUS - main.py updated to set groups for asteroid and create static containers for asteroid and asteroidfield. Also created a new object asteroid_field

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+from constants import LINE_WIDTH
+from circleshape import CircleShape
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def draw(self, surface):
+        pygame.draw.circle(surface, "white", self.position, self.radius, LINE_WIDTH)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 	pygame.init()
@@ -9,7 +11,13 @@ def main():
 	dt = 0
 	updatable  = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
-	Player.containers = (updatable , drawable)
+	asteroids = pygame.sprite.Group()
+
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = updatable
+	asteroid_field = AsteroidField()
+	
+	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	
 	print("Starting Asteroids with pygame version:", pygame.version.ver)
@@ -24,12 +32,18 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+			
 		log_state()
+
 		screen.fill("black")
+
 		for obj in drawable:
 			obj.draw(screen)
+
 		pygame.display.flip()
+
 		updatable.update(dt)
+		
 	
 		
 
